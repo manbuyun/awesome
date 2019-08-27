f5fc865b34a8baea0337ffa75b7a10848dc50b66
wangjinhai <wangjinhai@qutoutiao.net>
2019-08-27 17:00:20 +0800

    flink1.9.0

--- src/main/java/com/manbuyun/awesome/Main.java
+++ src/main/java/com/manbuyun/awesome/Main.java
@@ -1,12 +1,5 @@
 package com.manbuyun.awesome;
 
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeFormatterBuilder;
-import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.DateTimePrinter;
-
 /**
  * @author jinhai
  * @date 2019/05/10
@@ -14,18 +7,6 @@ import org.joda.time.format.DateTimePrinter;
 public class Main {
 
     public static void main(String[] args) {
-        DateTimeParser[] parsers = {
-                DateTimeFormat.forPattern("yyyy-MM-dd").getParser(),
-                DateTimeFormat.forPattern("yyyy-MM-dd HH").getParser(),
-                DateTimeFormat.forPattern("yyyy-MM-dd HH:mm").getParser(),
-                DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").getParser()};
-        DateTimePrinter printer = DateTimeFormat.forPattern("yyyy-MM-dd").getPrinter();
-        DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder()
-                .append(printer, parsers)
-                .toFormatter();
 
-        DateTime dateTime = dateTimeFormatter.parseDateTime("2019-05-05 19:00");
-
-        System.out.println(dateTime.minusDays(6).toString("yyyy-MM-dd HH:mm:ss"));
     }
 }
---
+++ src/main/java/com/manbuyun/awesome/common/DateUtils.java
@@ -0,0 +1,31 @@
+package com.manbuyun.awesome.common;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+import org.joda.time.format.DateTimeParser;
+import org.joda.time.format.DateTimePrinter;
+
+/**
+ * @author jinhai
+ * @date 2019/08/27
+ */
+public class DateUtils {
+
+    public static void main(String[] args) {
+        DateTimeParser[] parsers = {
+                DateTimeFormat.forPattern("yyyy-MM-dd").getParser(),
+                DateTimeFormat.forPattern("yyyy-MM-dd HH").getParser(),
+                DateTimeFormat.forPattern("yyyy-MM-dd HH:mm").getParser(),
+                DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").getParser()};
+        DateTimePrinter printer = DateTimeFormat.forPattern("yyyy-MM-dd").getPrinter();
+        DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder()
+                .append(printer, parsers)
+                .toFormatter();
+
+        DateTime dateTime = dateTimeFormatter.parseDateTime("2019-05-05 19:00");
+
+        System.out.println(dateTime.minusDays(6).toString("yyyy-MM-dd HH:mm:ss"));
+    }
+}
