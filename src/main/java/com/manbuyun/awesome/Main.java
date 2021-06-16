d2e67e3d4dc06104df1536c7bb6b8dc39526578c
jinhai <jinhai.me@gmail.com>
2021-06-16 11:41:48 +0800

    cleanup

--- src/main/java/com/manbuyun/awesome/Main.java
+++ src/main/java/com/manbuyun/awesome/Main.java
@@ -1,9 +1,12 @@
 package com.manbuyun.awesome;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author jinhai
  * @date 2019/05/10
  */
+@Slf4j
 public class Main {
 
     public static void main(String[] args) {
